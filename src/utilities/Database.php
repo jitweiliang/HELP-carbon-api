1bfd16f0fec74960a470088be3b114098253a5d8
ernestliang <103881112+ernestliang@users.noreply.github.com>
2024-04-02 23:15:12 +0800

    reset datababse connection to live

--- src/utilities/Database.php
+++ src/utilities/Database.php
@@ -3,46 +3,46 @@
 
     class Database {
         // // // This is the CLOUD one
-        private $host     = '35.186.148.24';
-        private $db_name  = 'carbon-db';
-        private $username = 'root';
-        private $password = 'Aeiou321';
+        // private $host     = '35.186.148.24';
+        // private $db_name  = 'carbon-db';
+        // private $username = 'root';
+        // private $password = 'Aeiou321';
 
-        private $conn;
-        public function getPDOObject(): PDO {
-            $this->conn = null;
-            try { 
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } 
-            catch(PDOException $e) {
-           throw new Exception($e->getMessage());
-            }
+        // private $conn;
+        // public function getPDOObject(): PDO {
+        //     $this->conn = null;
+        //     try { 
+        //         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        //         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     } 
+        //     catch(PDOException $e) {
+        //    throw new Exception($e->getMessage());
+        //     }
         
-            return $this->conn;
-        }
+        //     return $this->conn;
+        // }
 
 
 
         // --- 2. ==================== cloud sql via app engine ==================== --- //
-        // private $conn;
-        // private $host       = '/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db';
-        // private $username   = "root";
-        // private $password   = "Aeiou321";
-        // private $dbname     = "carbon-db";
+        private $conn;
+        private $host       = '/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db';
+        private $username   = "root";
+        private $password   = "Aeiou321";
+        private $dbname     = "carbon-db";
         
-        // public function getPDOObject(): PDO {
-        //     $this->conn = null;
-        //     try { 
-        //         // 'mysql:dbname=carbon-db;unix_socket=/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db'
+        public function getPDOObject(): PDO {
+            $this->conn = null;
+            try { 
+                // 'mysql:dbname=carbon-db;unix_socket=/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db'
                 
-        //         $connString = "mysql:dbname={$this->dbname};unix_socket={$this->host}";
-        //         $this->conn = new PDO($connString, $this->username, $this->password);
-        //     } 
-        //     catch(PDOException $e) {
-        //         throw new Exception($e->getMessage());
-        //     }
+                $connString = "mysql:dbname={$this->dbname};unix_socket={$this->host}";
+                $this->conn = new PDO($connString, $this->username, $this->password);
+            } 
+            catch(PDOException $e) {
+                throw new Exception($e->getMessage());
+            }
 
-        //     return $this->conn;
-        // }
+            return $this->conn;
+        }
   }
